304fab2e14f0b3b7f10b5b8aa6054dc33306c91a
Cc极生 <ccgeetion@gmail.com>
2016-07-13 03:05:00 +0800

    如果识别失败，重复请求数据

--- ScoreAPI/ScoreAPI.py
+++ ScoreAPI/ScoreAPI.py
@@ -11,64 +11,81 @@ from PIL import Image,ImageEnhance,ImageFilter,ImageDraw
 import pytesser
 import cookielib
 import cStringIO
+from bs4 import BeautifulSoup
 
-# def captchaRecognize(img):
-#     img = img.filter(ImageFilter.MedianFilter())
-#     img = img.filter(ImageFilter.MedianFilter())
-#     img = img.filter(ImageFilter.MedianFilter())
-#     img = img.filter(ImageFilter.MedianFilter())
-#     img = img.filter(ImageFilter.MedianFilter())
-#     img = img.filter(ImageFilter.MedianFilter())
-#     img = img.filter(ImageFilter.MedianFilter())
-#     img = img.filter(ImageFilter.MedianFilter())
-#
-#
-#     enhancer = ImageEnhance.Contrast(img)
-#     img = enhancer.enhance(2)
-#     img = img.convert('L')
-#     enhancer=ImageEnhance.Color(img);
-#     enhancer=enhancer.enhance(0);   #变成黑白
-#     enhancer=ImageEnhance.Contrast(enhancer);
-#     enhancer=enhancer.enhance(8);   #提高对比度
-#     # enhancer=ImageEnhance.Sharpness(enhancer);
-#     # enhancer=enhancer.enhance(20);  #锐化
-#     img = enhancer
-#
-#     img.show()
-#     text = pytesser.image_to_string(img)
-#     print text
-#     captach = re.findall('[0-9]',text)
-#     captcha =  ''.join(captach)
-#     return captcha
-'''''模拟登录'''
-reload(sys)
-sys.setdefaultencoding("utf-8")
-# 防止中文报错
-CaptchaUrl = "http://jwxt.ecjtu.jx.cn/servlet/code.servlet"
-PostUrl = 'http://jwxt.ecjtu.jx.cn/stuMag/Login_login.action'
-# 验证码地址和post地址
+
+def captchaRecognize(img):
+    #无限中值过滤噪点
+    img = img.filter(ImageFilter.MedianFilter())
+    img = img.filter(ImageFilter.MedianFilter())
+    img = img.filter(ImageFilter.MedianFilter())
+    img = img.filter(ImageFilter.MedianFilter())
+    img = img.filter(ImageFilter.MedianFilter())
+    img = img.filter(ImageFilter.MedianFilter())
+    img = img.filter(ImageFilter.MedianFilter())
+    img = img.filter(ImageFilter.MedianFilter())
+    #锐化等处理图片
+    enhancer = ImageEnhance.Contrast(img)
+    img = enhancer.enhance(2)
+    img = img.convert('L')
+    enhancer=ImageEnhance.Color(img);
+    enhancer=enhancer.enhance(0);   #变成黑白
+    enhancer=ImageEnhance.Contrast(enhancer);
+    enhancer=enhancer.enhance(8);   #提高对比度
+    img = enhancer
+    #识别修改后的图片
+    text = pytesser.image_to_string(img)
+    #使用正则表达式过滤除数字以外的识别数据
+    captach = re.findall('[0-9]',text)
+    captcha =  ''.join(captach)
+    return captcha
+
+def loginRequest(username,password,headers,opener):
+    # 验证码地址和post地址
+    CaptchaUrl = "http://jwxt.ecjtu.jx.cn/servlet/code.servlet"
+    PostUrl = 'http://jwxt.ecjtu.jx.cn/stuMag/Login_login.action'
+    picture = opener.open(CaptchaUrl).read()
+    # 用openr访问验证码地址,获取cookie
+    local = open('cod.jpg', 'wb')
+    local.write(picture)
+    local.close()
+    # 保存验证码到本地
+    img = Image.open('cod.jpg')
+    SecretCode = captchaRecognize(img)
+
+    postData = {
+            'UserName':username,
+            'Password': password,
+            'code':SecretCode
+    }
+    data = urllib.urlencode(postData)
+    # 生成post数据 ?key1=value1&key2=value2的形式
+    request = urllib2.Request(PostUrl, data, headers)
+    # 构造request请求
+    # 打开保存的验证码图片 输入
+    try:
+        response = opener.open(request)
+        result = response.read()
+        if result == '验证码错误':
+            loginRequest(PostUrl,CaptchaUrl,headers,opener)
+        else:
+            return result
+        # 打印登录后的页面
+    except urllib2.HTTPError, e:
+        print e.code
+
+def regular(result):
+    soup = BeautifulSoup(result)
+    table = soup.find_all('table')
+    return table
+# 用户名和密码
+username = '2015211001000116'
+password = 'qq3396675'
+# 将cookies绑定到一个opener cookie由cookielib自动管理
 cookie = cookielib.CookieJar()
 handler = urllib2.HTTPCookieProcessor(cookie)
 opener = urllib2.build_opener(handler)
-# 将cookies绑定到一个opener cookie由cookielib自动管理
-username = '2015211001000116'
-password = 'qq3396675'
-# 用户名和密码
-picture = opener.open(CaptchaUrl).read()
-# 用openr访问验证码地址,获取cookie
-local = open('cod.jpg', 'wb')
-local.write(picture)
-local.close()
-# 保存验证码到本地
-img = Image.open('cod.jpg')
-SecretCode = raw_input('输入')
-# 打开保存的验证码图片 输入
-postData = {
-        'UserName':username,
-        'Password': password,
-        'code':SecretCode
-}
-# 根据抓包信息 构造表单
+
 headers = {
          'Accept':'*/*' ,
          'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
@@ -79,16 +96,7 @@ headers = {
          'User-Agent':'Mozilla/5.0(Windows NT 6.1;WOW64;Trident/7.0;rv:11.0)like Gecko',
          'Host':'jwxt.ecjtu.jx.cn',
 }
-# 根据抓包信息 构造headers
-data = urllib.urlencode(postData)
-# 生成post数据 ?key1=value1&key2=value2的形式
-request = urllib2.Request(PostUrl, data, headers)
-# 构造request请求
-try:
-    response = opener.open(request)
-    result = response.read().decode('gb2312')
-    # 由于该网页是gb2312的编码，所以需要解码
-    print result
-    # 打印登录后的页面
-except urllib2.HTTPError, e:
-    print e.code
+loginRequest(username,password,headers,opener)
+response = opener.open('http://jwxt.ecjtu.jx.cn/scoreQuery/stuScoreQue_getStuScore.action')
+result = response.read()
+print regular(result)
